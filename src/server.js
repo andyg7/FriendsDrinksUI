d590e7edc4dd80cc3114e86302cf803675e3adbe
Andrew Grant <andrewgrant243@gmail.com>
2020-10-06 21:11:01 -0400

    Split up big method

--- src/server.js
+++ src/server.js
@@ -95,8 +95,8 @@ function createServer(userManagement, backendConfig) {
                                 invitations.push(
                                    {
                                       message: item.message,
-                                      adminUserId: item.adminUserId,
-                                      friendsDrinksId: item.uuid
+                                      adminUserId: item.friendsDrinksId.adminUserId,
+                                      friendsDrinksUuid: item.friendsDrinksId.uuid
                                    }
                                 )
                             });
@@ -195,7 +195,7 @@ function createServer(userManagement, backendConfig) {
           return;
         })
 
-        app.post('/friendsdrinks', function (req, res) {
+        app.post('/friendsdrinks/removeUser', function (req, res) {
             const sessionId = req.cookies[SESSION_KEY];
             if (!sessionId) {
                 res.redirect('/login')
@@ -211,44 +211,188 @@ function createServer(userManagement, backendConfig) {
                     return;
                 } else {
                     let postObj = null;
-                    let path = null;
-                    if (req.body.id) {
-                      // Update FriendsDrinks
-                      if (req.body.name) {
-                        // "Normal" update of FriendsDrinks
-                        postObj = {
-                          name: req.body.name
-                        }
-                        path = "/v1/users/" + username + "/adminfriendsdrinks/" + req.body.id
-                      } if (req.body.userId) {
-                        // INVITE_FRIEND POST
-                        postObj = {
-                          userId: req.body.userId,
-                          eventType: 'INVITE_FRIEND',
-                          friendsDrinksId: {
-                              adminUserId: username,
-                              uuid: req.body.id
-                          }
-                        }
-                        path = "/v1/users/" + username
-                      } else {
-                        //  update
-                        postObj = {
-                          eventType: 'REPLY_TO_INVITATION',
-                          friendsDrinksId: {
-                              adminUserId: req.body.adminUserId,
-                              uuid: req.body.id
-                          },
-                          invitationReply: req.body.invitationReply
-                        }
-                        path = "/v1/users/" + username
+                    let path = "/v1/users/" + username
+                    postObj = {
+                      userId: username,
+                      eventType: 'REMOVE_USER',
+                      friendsDrinksId: {
+                          adminUserId: username,
+                          uuid: req.body.id
                       }
-                    } else {
-                      // Create request
-                      path = "/v1/users/" + username + "/adminfriendsdrinks"
-                      postObj = {
-                          name: req.body.name
-                     }
+                    }
+                    const postData = JSON.stringify(postObj)
+                    let options = {
+                      host: backendHostname,
+                      port: backendPort,
+                      path: path,
+                      method: 'POST',
+                      headers: {
+                          'Content-Length': Buffer.byteLength(postData),
+                          'Content-Type': 'application/json'
+                      }
+                    };
+
+                    let backendReq = buildHttpPostRequest(options, res)
+                    console.log("Sending request", postData)
+                    backendReq.write(postData);
+                    backendReq.end();
+                    return;
+                }
+            }
+        })
+
+        app.post('/friendsdrinks/addUser', function (req, res) {
+            const sessionId = req.cookies[SESSION_KEY];
+            if (!sessionId) {
+                res.redirect('/login')
+                return;
+             } else {
+                const username = userManagement.getLoggedInUser(sessionId);
+                if (username === null) {
+                    res.cookie(SESSION_KEY, "", {
+                        path: '/',
+                        expires: new Date(1)
+                    });
+                    res.redirect('/login')
+                    return;
+                } else {
+                    let postObj = {
+                      userId: req.body.userId,
+                      eventType: 'ADD_USER',
+                      friendsDrinksUuid: req.body.id
+                    }
+                    let path = "/v1/users/" + username
+
+                    const postData = JSON.stringify(postObj)
+
+                    let options = {
+                      host: backendHostname,
+                      port: backendPort,
+                      path: path,
+                      method: 'POST',
+                      headers: {
+                          'Content-Length': Buffer.byteLength(postData),
+                          'Content-Type': 'application/json'
+                      }
+                    };
+
+                    let backendReq = buildHttpPostRequest(options, res)
+
+                    console.log("Sending request", postData)
+                    backendReq.write(postData);
+                    backendReq.end();
+                    return;
+                }
+            }
+        })
+
+        app.post('/friendsdrinks/replyToInvitation', function (req, res) {
+            const sessionId = req.cookies[SESSION_KEY];
+            if (!sessionId) {
+                res.redirect('/login')
+                return;
+             } else {
+                const username = userManagement.getLoggedInUser(sessionId);
+                if (username === null) {
+                    res.cookie(SESSION_KEY, "", {
+                        path: '/',
+                        expires: new Date(1)
+                    });
+                    res.redirect('/login')
+                    return;
+                } else {
+                    let postObj = {
+                      eventType: 'REPLY_TO_INVITATION',
+                      adminUserId: req.body.adminUserId,
+                      friendsDrinksUuid: req.body.id,
+                      invitationReply: req.body.invitationReply
+                    }
+                    let path = "/v1/users/" + username
+
+                    const postData = JSON.stringify(postObj)
+
+                    let options = {
+                      host: backendHostname,
+                      port: backendPort,
+                      path: path,
+                      method: 'POST',
+                      headers: {
+                          'Content-Length': Buffer.byteLength(postData),
+                          'Content-Type': 'application/json'
+                      }
+                    };
+
+                    let backendReq = buildHttpPostRequest(options, res)
+
+                    console.log("Sending request", postData)
+                    backendReq.write(postData);
+                    backendReq.end();
+                    return;
+                }
+            }
+        })
+
+        app.post('/friendsdrinks/update', function (req, res) {
+            const sessionId = req.cookies[SESSION_KEY];
+            if (!sessionId) {
+                res.redirect('/login')
+                return;
+             } else {
+                const username = userManagement.getLoggedInUser(sessionId);
+                if (username === null) {
+                    res.cookie(SESSION_KEY, "", {
+                        path: '/',
+                        expires: new Date(1)
+                    });
+                    res.redirect('/login')
+                    return;
+                } else {
+                    let postObj = {
+                      name: req.body.name
+                    }
+                    let path = "/v1/users/" + username + "/adminfriendsdrinks/" + req.body.id
+
+                    const postData = JSON.stringify(postObj)
+
+                    let options = {
+                      host: backendHostname,
+                      port: backendPort,
+                      path: path,
+                      method: 'POST',
+                      headers: {
+                          'Content-Length': Buffer.byteLength(postData),
+                          'Content-Type': 'application/json'
+                      }
+                    };
+
+                    let backendReq = buildHttpPostRequest(options, res)
+
+                    console.log("Sending request", postData)
+                    backendReq.write(postData);
+                    backendReq.end();
+                    return;
+                }
+            }
+        })
+
+        app.post('/friendsdrinks/create', function (req, res) {
+            const sessionId = req.cookies[SESSION_KEY];
+            if (!sessionId) {
+                res.redirect('/login')
+                return;
+             } else {
+                const username = userManagement.getLoggedInUser(sessionId);
+                if (username === null) {
+                    res.cookie(SESSION_KEY, "", {
+                        path: '/',
+                        expires: new Date(1)
+                    });
+                    res.redirect('/login')
+                    return;
+                } else {
+                    let path = "/v1/users/" + username + "/adminfriendsdrinks"
+                    let postObj = {
+                      name: req.body.name
                     }
                     const postData = JSON.stringify(postObj)
                     console.log(path)
@@ -262,31 +406,7 @@ function createServer(userManagement, backendConfig) {
                           'Content-Type': 'application/json'
                       }
                     };
-                    let backendReq = http.request(options, function(backendRes) {
-                      console.log('STATUS: ' + backendRes.statusCode);
-                      console.log('HEADERS: ' + JSON.stringify(backendRes.headers));
-                      if (backendRes.statusCode !== 200) {
-                         backendRes.resume();
-                         res.status(500);
-                         res.send("Whoops! Something went wrong :(");
-                         return;
-                      } else {
-                          let bodyChunks = [];
-                          backendRes.on('data', (chunk) => {
-                            bodyChunks.push(chunk)
-                          });
-                          backendRes.on('end', () => {
-                            let body = Buffer.concat(bodyChunks);
-                            console.log('BODY: ' + body);
-                            const obj = JSON.parse(body);
-                            console.log('Result ', obj.result);
-
-                            console.log('No more data in response - redirecting to /');
-                            res.redirect('/')
-                            return;
-                          });
-                      }
-                    });
+                    let backendReq = buildHttpPostRequest(options, res)
 
                     backendReq.on('error', function(e) {
                       console.log('ERROR: ' + e.message);
@@ -302,6 +422,43 @@ function createServer(userManagement, backendConfig) {
                 }
             }
         })
+
+        function buildHttpPostRequest(options, res) {
+            let backendReq = http.request(options, function(backendRes) {
+              console.log('STATUS: ' + backendRes.statusCode);
+              console.log('HEADERS: ' + JSON.stringify(backendRes.headers));
+              if (backendRes.statusCode !== 200) {
+                 backendRes.resume();
+                 res.status(500);
+                 res.send("Whoops! Something went wrong :(");
+                 return;
+              } else {
+                  let bodyChunks = [];
+                  backendRes.on('data', (chunk) => {
+                    bodyChunks.push(chunk)
+                  });
+                  backendRes.on('end', () => {
+                    let body = Buffer.concat(bodyChunks);
+                    console.log('BODY: ' + body);
+                    const obj = JSON.parse(body);
+                    console.log('Result ', obj.result);
+
+                    console.log('No more data in response - redirecting to /');
+                    res.redirect('/')
+                    return;
+                  });
+              }
+            });
+
+            backendReq.on('error', function(e) {
+              console.log('ERROR: ' + e.message);
+              res.status(500);
+              res.send("Whoops! Something went wrong :(");
+              return;
+            });
+
+            return backendReq;
+        }
 
         app.post('/signup', function (req, res) {
             let email = req.body.email;
@@ -459,6 +616,7 @@ function createServer(userManagement, backendConfig) {
                 return;
             });
         })
+
         return app
 }
 
