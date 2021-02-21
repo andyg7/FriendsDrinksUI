03d193014a53221be5752b6b7d2fbaeea6eac28d
Andrew Grant <andrewgrant243@gmail.com>
2021-02-21 12:48:53 -0500

    Remove ability to override client ID and user pool ID path

--- src/app.js
+++ src/app.js
@@ -29,29 +29,15 @@ let backendConfig = {}
 backendConfig.hostname = properties.get('backendHostname')
 backendConfig.port = properties.get('backendPort')
 
-let defaultClientIdPath = properties.get('clientIdPath')
-let defaultUserPoolIdPath = properties.get('userPoolIdPath')
+let clientIdPath = properties.get('clientIdPath')
+let userPoolIdPath = properties.get('userPoolIdPath')
 
 console.log('Backend hostname: ' + backendConfig.hostname)
 console.log('Backend port: ' + backendConfig.port)
 
-let clientIdPath;
-if (argv['client-id-path']) {
-  clientIdPath = argv['client-id-path']
-} else {
-  // Default config location
-  clientIdPath = defaultClientIdPath
-}
 const clientId = fs.readFileSync(clientIdPath, {encoding:'utf8', flag:'r'});
 console.log("Client ID:" + clientId)
 
-let userPoolIdPath;
-if (argv['user-pool-id-path']) {
-  userPoolIdPath = argv['user-pool-id-path']
-} else {
-  // Default config location
-  userPoolIdPath = defaultUserPoolIdPath
-}
 const userPoolId = fs.readFileSync(userPoolIdPath, {encoding:'utf8', flag:'r'});
 console.log("User pool ID:" + userPoolId)
 
