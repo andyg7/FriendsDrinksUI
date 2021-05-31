03c4d69febba5fd422ea815790f12120777d9a3c
Andrew Grant <andrewgrant243@gmail.com>
2021-05-31 09:53:56 -0400

    Automate ssl registration and verification

--- semaphore/register_dns.sh
+++ semaphore/register_dns.sh
@@ -2,22 +2,24 @@
 
 set -eu
 
-aws sts get-caller-identity
-
 stage=$1
 region=$2
+file=$3
 
 domainNameToRegister="${stage}.friendsdrinks.com"
 echo "Domain name to register ${domainNameToRegister}"
 domain=$(aws --region "$region" acm list-certificates --query 'CertificateSummaryList[].DomainName' --output text | grep "${stage}.friendsdrinks.com" || echo 'na')
-echo 'bye'
 
 if [ "$domain" = 'na' ]
 then
       echo "About to attempt to register domain name ${domainNameToRegister}"
-      aws --region $region acm request-certificate --domain-name "$domainNameToRegister" --validation-method DNS
+      certificateArn=$(aws --region "$region" acm request-certificate --domain-name "$domainNameToRegister" --validation-method DNS --query 'CertificateArn' --output text)
+      echo "$certificateArn"
+      sleep 5
+      aws --region "$region" acm describe-certificate --certificate-arn "$certificateArn" --query 'Certificate.DomainValidationOptions[0].ResourceRecord' | tee "$file"
 else
       echo "${domainNameToRegister} is already registered"
+      echo 'na' > "$file"
       exit 0
 fi
 
---
+++ semaphore/validate_dns_ownership.sh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+set -eu
+
+cname_file=$1
+
+record_set=$(cat $cname_file)
+
+
+if [ "$record_set" = 'na' ]
+then
+  exit 0
+else
+  name=$(cat $cname_file | jq -r '.Name')
+  echo "Name is ${name}"
+  value=$(cat $cname_file | jq -r '.Value')
+  echo "Value is ${value}"
+  aws cloudformation validate-template --template-body file://cloudformation/dns/cname.yml
+  aws cloudformation deploy --template-file cloudformation/dns/cname.yml --stack-name CNAMERecordSetStack --parameter-overrides CNAMERecordValue="$value" CNAMERecordName="$name" HostedZoneId=Z03368065P2HEQ3Z6RIO Stage=beta
+fi
