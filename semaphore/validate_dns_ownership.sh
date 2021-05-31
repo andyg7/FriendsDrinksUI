3ec0a45fb2d2a656676e248f0a56a60226e50505
Andrew Grant <andrewgrant243@gmail.com>
2021-05-31 10:33:13 -0400

    List resource record sets for debugging

--- semaphore/validate_dns_ownership.sh
+++ semaphore/validate_dns_ownership.sh
@@ -2,18 +2,19 @@
 
 set -eu
 
-cname_file=$1
+stage=$1
+cname_file=$2
 
-record_set=$(cat $cname_file)
+record_set=$(cat "$cname_file")
 
 
 if [ "$record_set" = 'na' ]
 then
   exit 0
 else
-  name=$(cat $cname_file | jq -r '.Name')
+  name=$(cat "$cname_file" | jq -r '.Name')
   echo "Name is ${name}"
-  value=$(cat $cname_file | jq -r '.Value')
+  value=$(cat "$cname_file" | jq -r '.Value')
   echo "Value is ${value}"
   aws cloudformation validate-template --template-body file://cloudformation/dns/cname.yml
   aws cloudformation deploy --template-file cloudformation/dns/cname.yml --stack-name CNAMERecordSetStack --parameter-overrides CNAMERecordValue="$value" CNAMERecordName="$name" HostedZoneId=Z03368065P2HEQ3Z6RIO Stage=beta
