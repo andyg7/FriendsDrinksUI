d3c71783941763e8a53f0ebb060761bd320f26ba
Andrew Grant <andrewgrant243@gmail.com>
2021-06-22 18:01:07 -0400

    Create all resources via script

--- kubernetes/exec_all_services.sh
+++ kubernetes/exec_all_services.sh
@@ -4,8 +4,15 @@ set -eu
 
 action=$1
 
+kubectl $action -f kubernetes/10config.yml
+kubectl $action -f kubernetes/10secret.yml
+
 apply_tmp=$(mktemp)
 ./kubernetes/generate_manifest.sh kubernetes/21app.yml andyg001/friendsdrinksui:latest apply_tmp
+kubectl $action -f apply_tmp
+
+apply_tmp=$(mktemp)
+./kubernetes/generate_manifest.sh kubernetes/20service.yml andyg001/friendsdrinksui:latest apply_tmp
 kubectl $action -f apply_tmp
 
 rm -rf apply_tmp
