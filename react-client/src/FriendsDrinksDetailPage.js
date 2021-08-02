4c516a84c589552272274a9cbf7c4a4157d10c93
Andrew Grant <andrewgrant243@gmail.com>
2021-08-01 20:17:12 -0400

    Create dummy FriendsDrinks homepage

---
+++ react-client/src/FriendsDrinksDetailPage.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+
+export default class FriendsDrinksDetailPage extends React.Component {
+
+    render() {
+        return <h1>FriendsDrinks detail page!</h1>;
+    }
+}
---
+++ react-client/src/Homepage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            homepageDto: {
+                name: "Andrew",
+                friendsDrinksList: [{
+                    name: "Whiskey drinks"
+                }]
+            }
+        };
+    }
+
+    render() {
+        const dto = this.state.homepageDto;
+        const friendsDrinksListItem = dto.friendsDrinksList.map((x) => <li>{x.name}</li>);
+        return (
+            <div>
+                <h1>Welcome back {this.state.homepageDto.name}!</h1>
+                <h2>Your FriendsDrinkses!</h2>
+                <ul>{friendsDrinksListItem}</ul>
+            </div>
+        );
+    }
+}
