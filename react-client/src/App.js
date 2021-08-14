e519d6ce3285134a2b3183a1971935d6bc710a19
Andrew Grant <andrewgrant243@gmail.com>
2021-08-13 23:01:57 -0400

    Fix import

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import LoginControl from './Auth';
+import LoginControl from './LoginControl';
 import LoggedInApp from './LoggedInApp';
 
 class App extends React.Component {
--- react-client/src/FriendsDrinksDetailPage.js
+++ react-client/src/FriendsDrinksDetailPage.js
@@ -5,6 +5,7 @@ export default class FriendsDrinksDetailPage extends React.Component {
 
     render() {
         const friendsDrinksId = this.props.friendsDrinksId;
+        console.log("Friends drinks ID is " + friendsDrinksId);
         // API call to get data and display here.
         return <h1>FriendsDrinks detail page!</h1>;
     }
