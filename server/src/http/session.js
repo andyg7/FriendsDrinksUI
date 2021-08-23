bc4e904727bc7696c9bc6a5a76c77a6d2557ef62
Andrew Grant <andrewgrant243@gmail.com>
2021-08-23 08:09:32 -0400

    Add missing session files

---
+++ server/src/http/session.js
@@ -0,0 +1,22 @@
+
+class SessionIdExtractor {
+
+    constructor(sessionKey) {
+        this.sessionKey = sessionKey
+    }
+
+    getSessionId(req) {
+        let sessionId = req.cookies[this.sessionKey];
+        if (!sessionId) {
+            if (!req.body) {
+                return req.body.sessionId;
+            } else {
+                return null;
+            }
+        } else {
+            return sessionId;
+        }
+    }
+}
+
+module.exports = SessionIdExtractor
